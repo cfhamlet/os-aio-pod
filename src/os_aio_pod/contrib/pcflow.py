80997c8c85c242b81dd7f5ad1297861dddee11f0
Ozzy <cfhamlet@gmail.com>
2021-01-06 15:32:33 +0800

    ref: stop

--- src/os_aio_pod/contrib/pcflow.py
+++ src/os_aio_pod/contrib/pcflow.py
@@ -12,16 +12,10 @@ class Server(BaseServer):
 
     async def run(self, **kwargs):
         await super(Server, self).run(**kwargs)
-        d = {"stop": False}
+
         consumer_num = kwargs.get("consumer_num", 10)
         queue_size = kwargs.get("queue_size", 10)
         queue = asyncio.Queue(maxsize=queue_size)
-
-        def _on_stop(**kw):
-            d["stop"] = True
-
-        for signal in ("SIGTERM", "SIGINT"):
-            await self.context.add_signal_handler(signal, _on_stop)
 
         class Stop:
             pass
@@ -36,8 +30,6 @@ class Server(BaseServer):
         async def _produce():
             async for obj in self.produce(**kwargs):
                 await queue.put(obj)
-                if d["stop"]:
-                    break
             for _ in range(consumer_num):
                 await queue.put(Stop())
 
